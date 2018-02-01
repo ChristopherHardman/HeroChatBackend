eda5d3b82dbf0fef5d7908f4e6255bce12e900a7
ChristopherHardman <cwhardman@gmail.com>
2018-02-01 00:50:13 +0000

    first commit

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const UserModel = require ('./Model/chatModel');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/chat', {useMongoClient: true});
+mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds121118.mlab.com:21118/heroku_gzk5t4gv', {useMongoClient: true});
 mongoose.connection.on('connected', () => {
   console.log('Mongoose connected')
 })
